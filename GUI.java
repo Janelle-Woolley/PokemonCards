91b3cbcc53d4179ef99ab849ec4eb103df4c347c
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2023-05-10 14:31:12 +1200

    Added askName() method
    Ask for input
    Sanitises input
    Returns input
    
    Added quit button

--- GUI.java
+++ GUI.java
@@ -14,21 +14,40 @@ public class GUI {
   /**
    * Constructor for objects of class GUI.
    */
-  public GUI()
-  {
+  public GUI() {
     cardStack = new CardHashMap(); // initialise the hashmap
     UI.initialise(); // initialise GUI
     
     // create buttons
-      // search button
-      // quit button
+    UI.addButton("Quit", UI::quit);  // quit button
       // add card button
       // hide details button
       // show all cards details buttons
+      // search button
     // create mouse listener
   }
   
-  // method to get name input
+  /**
+   * Asks user for card name input.
+   */
+  public String askName() {
+    boolean gettingName = true; // boolean for looping
+    String name = UI.askString("Enter the card name: "); // ask for input
+    
+    // loops until the input is valid
+    while (gettingName) {
+      // checks if the input is just whitespaces
+      if (name.isBlank()) {
+        name = UI.askString("Enter the card name: ");
+      } else { // otherwise input is valid and loop ends
+        gettingName = false;
+      }
+    }
+    return name; // returns input
+    
+    // TO ADD:
+    // checks if the input is already in the hashmap
+  }
   // method to get price input
   // method to get image input
   // method to add inputs to hashmap
